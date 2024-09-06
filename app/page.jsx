a6124bcc179b2062af06ebac7176499565288f86
deepak1051 <deepakpanwar1051@gmail.com>
2024-09-06 17:53:03 +0530

    add tailwind and daisy ui

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 export default function Page() {
   return (
     <div>
-      <h2>hello</h2>
+      <h2 className="text-3xl text-blue-400">hello</h2>
+
+      <Link href="/login" className="btn">
+        login page
+      </Link>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo architecto
         non ab omnis unde, quia ullam expedita, tempore explicabo esse qui dicta
