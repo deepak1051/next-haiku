da50cd653e962b80fb8d4c1a81d9e76c5a1b898c
deepak1051 <deepakpanwar1051@gmail.com>
2024-09-06 18:41:48 +0530

    add form validation

--- actions/userController.js
+++ actions/userController.js
@@ -1,9 +1,47 @@
 'use server';
 
-export const register = async () => {
-  console.log('hello this is from server');
+function isAlphanumeric(str) {
+  const regex = /^[a-zA-Z0-9]*$/;
+  return regex.test(str);
+}
+
+export const register = async (prevState, formData) => {
+  const errors = {};
+
+  const ourUser = {
+    username: formData.get('username'),
+    password: formData.get('password'),
+  };
+
+  if (typeof ourUser.username != 'string') ourUser.username = '';
+  if (typeof ourUser.password != 'string') ourUser.password = '';
+
+  ourUser.username = ourUser.username.trim();
+  ourUser.password = ourUser.password.trim();
+
+  if (ourUser.username.length < 3)
+    errors.username = 'Username must be atleast 3 chars';
+  if (ourUser.username.length > 20)
+    errors.username = 'Username cannot exceed 20 chars';
+  if (!isAlphanumeric(ourUser.username))
+    errors.username = 'Username should be alpha numeric';
+  if (ourUser.username === '') errors.username = 'You must provide a username';
+
+  if (ourUser.password.length < 4)
+    errors.password = 'Password must be atleast 4 chars';
+  if (ourUser.password.length > 20)
+    errors.password = 'Password cannot exceed 20 chars';
+
+  if (ourUser.password === '') errors.password = 'You must provide a password';
+
+  if (errors.username || errors.password) {
+    return {
+      errors: errors,
+      success: false,
+    };
+  }
 
   return {
-    light: 'yagami',
+    success: true,
   };
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { register } from '../actions/userController';
+import RegisterForm from '../components/RegisterForm';
 
 export default function Page() {
   return (
@@ -8,29 +9,7 @@ export default function Page() {
         Don&rsquo;t have an account <strong>Create One</strong>
       </p>
 
-      <form action={register} className="max-w-xs mx-auto">
-        <div className="mb-3">
-          <input
-            name="username"
-            autoComplete={false}
-            type="text"
-            placeholder="Username"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>
-
-        <div className="mb-3">
-          <input
-            name="password"
-            autoComplete={false}
-            type="password"
-            placeholder="Password"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>
-
-        <button className="btn btn-primary">Create Account</button>
-      </form>
+      <RegisterForm />
     </>
   );
 }
