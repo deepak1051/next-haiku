930b4b86b7a389e6552ba70d6e42f6a877831e50
deepak1051 <deepakpanwar1051@gmail.com>
2024-09-06 17:04:35 +0530

    next js setup

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>hello</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo architecto
+        non ab omnis unde, quia ullam expedita, tempore explicabo esse qui dicta
+        atque reprehenderit beatae earum nobis ratione, fugit blanditiis?{' '}
+      </p>
+    </div>
+  );
+}
